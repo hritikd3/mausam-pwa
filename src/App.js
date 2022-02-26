ad8c60cd660d1fbd4717e507c8daf0bfc2c49112
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-26 19:30:12 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -29,6 +29,11 @@ const App = () => {
               <span> {weather.name}</span>
               <sup> {weather.sys.country}</sup>
             </h2>
+            <div className="city-temp">
+              {Math.round{weather.main.temp}}
+              <sup>&deg; C </sup>
+               </div>
+        
           </div>
         )}
       </div>
